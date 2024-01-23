e2ed426caca3d0a22254c0323f94db56e1e997a8
MrRodent <finaali@gmail.com>
2024-01-23 20:07:45 +0200

    Osa 1a: Monta komponenttia

---
+++ harjoitustehtävät/part1/src/App.jsx
@@ -0,0 +1,22 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+// ÄLÄ POISTA IKINÄ. Ilman tätä ohjelma hajoaa
+export default App
