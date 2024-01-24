c46faf51cc8b1675d63bd3bf420e8f391b432c0d
MrRodent <finaali@gmail.com>
2024-01-24 21:16:50 +0200

    kurssitiedot step4

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -2,8 +2,8 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
-const Part = (props) => {
-  return <p>{props.part} {props.exercises}</p>
+const Part = ({part, exercises}) => {
+  return <p>{part} {exercises}</p>
 }
 
 const Content = ({parts}) => {
@@ -17,30 +17,34 @@ const Content = ({parts}) => {
   )
 }
 
-const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>
+const Total = ({parts}) => {
+  let total = 0;
+  parts.forEach(part => {
+    total += part.exercises;
+  });
+  return <p>Number of exercises {total}</p>
 }
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
+  const parts = [{
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
-  }
+  }]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
 }
