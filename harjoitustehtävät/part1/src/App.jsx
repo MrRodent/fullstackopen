2307dc354d92b289a249750ebb4e7af078b07615
MrRodent <finaali@gmail.com>
2024-01-25 16:29:27 +0200

    harjoitus: destrukturointi

--- harjoitustehtävät/part1/src/App.jsx
+++ harjoitustehtävät/part1/src/App.jsx
@@ -1,19 +1,27 @@
-const Hello = () => {
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age;
+  
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>
+        So you were probably born {bornYear()}
+      </p>
     </div>
   )
 }
 
 const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
     </div>
   )
 }
