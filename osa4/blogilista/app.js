10c307246a35c0d0e8aeb8151b1318bfdd65a2a2
Tommi <83519567+MrRodent@users.noreply.github.com>
2024-10-13 23:28:27 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -7,6 +7,8 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+const listHelper = require('./utils/list_helper')
+
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB');
