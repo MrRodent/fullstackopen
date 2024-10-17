9a89925bf4c4467d388c9c0edc19c8639ad8339d
MrRodent <finaali@gmail.com>
2024-10-17 16:23:47 +0300

    note_api testing 2

--- harjoitustehtävät/part4/utils/logger.js
+++ harjoitustehtävät/part4/utils/logger.js
@@ -1,15 +1,9 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') { 
     console.log(...params)
-  }
 }
 
 const error = (...params) => {
-  if (process.env.NODE_ENV !== 'test') { 
-    console.error(...params)
-  }
+    console.log(...params);
 }
 
-module.exports = {
-  info, error
-}
+module.exports = { info, error }
